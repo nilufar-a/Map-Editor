1bf544de6b836f6a3bff5866b4672a67b0e1d61f
BrendaHer <45469500+BrendaHer@users.noreply.github.com>
2019-11-28 23:13:18 +0100

    It finally shows something on google cloud, i can also pass a map object as a json file if you aappend to the url /test

--- test/src/call/MapEditor.java
+++ test/src/call/MapEditor.java
@@ -3,23 +3,6 @@ import java.util.ArrayList;
 
 public class MapEditor {
 		
-		ArrayList<Map> maps;
-		
-		public MapEditor() {
-			super();
-			this.maps = new ArrayList<Map>();
-		}
-		
-		public void addMap(int width, int height, int numberOfPlayers, String name) {// exceptions! w,h, min max 0
-			
-			Map newMap = new Map(width, height, numberOfPlayers, maps.size(), name);
-			maps.add(newMap);
-			
-		}
-		
-		public void removeMap(int id) { // it doesn't check for duplicates yet tho
 
-					maps.remove(id);
-		}
 	
 }
--- test/src/call/RESTCall.java
+++ test/src/call/RESTCall.java
@@ -1,26 +1,69 @@
 package call;
 
-import java.awt.List;
-import java.util.ArrayList;
-
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.internal.guava.Maps;
+import org.glassfish.hk2.api.Immediate;
+import org.glassfish.jersey.process.internal.RequestScoped;
 
+import call.MapEditor;
 
-@Path("/map-editor")
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import javax.sound.midi.Track;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.core.Application;
+
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Singleton
+@Path("/")
 public class RESTCall {
-	MapEditor me = new MapEditor();
+	private ArrayList<Map> maps = new ArrayList<Map>();
+	
+	
+	public void addMap(int width, int height, int numberOfPlayers, String name) {// exceptions! w,h, min max 0
+		
+		Map newMap = new Map(width, height, numberOfPlayers, maps.size()+1, name);
+		maps.add(newMap);
+		
+	}
+	
+	public void removeMap(int id) { // it doesn't check for duplicates yet tho
+
+				maps.remove(id);
+	}
+	
+	 
 	 @GET
 	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
+	 @Path ("/test")
+	 public Map test() {
+		 addMap(64, 64, 4, "Stubydoobydoo");
+		 return maps.get(0);
+	 }
+	 /*
+	 
+	 @GET
+	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path ("/getMap")
 	 public Map getMap(@QueryParam ("MapID") int MapID) {
 		 return me.maps.get(MapID);
@@ -28,13 +71,7 @@ public class RESTCall {
 	 
 	 @GET
 	 @Produces(MediaType.APPLICATION_JSON)
-	 @Path ("/test")
-	 public String test() {
-		 return "I work";
-	 }
-	 
-	 @GET
-	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path ("/isNameOccupied")
 	 public Boolean isNameOccupied(@QueryParam ("mapName") String mapName) {
 		 Boolean isOccupied = Boolean.FALSE;
@@ -48,6 +85,7 @@ public class RESTCall {
 	 
 	 @GET
 	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path("/getMapsWithXPlayers/{NumberOfPlayers}")
 	 public Response getMapsWithXPlayers(@QueryParam("NumberOfPlayers") int NumberOfPlayers) {
 		 ArrayList<Map> temp = new ArrayList<Map>();
@@ -62,6 +100,7 @@ public class RESTCall {
 	 
 	 @GET
 	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path("/getAllMaps")
 	 public Response getAllMaps() {		 
 		 return Response.ok(me.maps).build();
@@ -69,6 +108,7 @@ public class RESTCall {
 	 
 	 @POST
 	 @Produces(MediaType.TEXT_HTML)
+	 @Singleton
 	 @Path("/uploadMap")
 	 public String uploadMap(@QueryParam("xPos") String xPos) {
 		 
@@ -77,6 +117,7 @@ public class RESTCall {
 	 
 	 @PUT
 	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path("/updateMap")
 	 public String updateMap(@QueryParam("id") int id, @QueryParam("mapGrid") String[][] mapgrid) {
 		 //
@@ -85,13 +126,14 @@ public class RESTCall {
 	 
 	 @DELETE
 	 @Produces(MediaType.APPLICATION_JSON)
+	 @Singleton
 	 @Path("/deleteMap")
 	 public String deleteMap(@QueryParam("id") int id) {
 
 		 me.maps.remove(id);
 		 
 		 return "success";
-	 }
+	 }*/
 	 
 
 }
